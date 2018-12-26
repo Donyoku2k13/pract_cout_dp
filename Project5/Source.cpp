4796a49653cb73c027e2cea5309c8928399ff8a7
Александр Горячев <@local.itstep>
2018-12-26 20:59:28 +0600

    Добавьте файлы проекта.

---
+++ Project5/Source.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+#include<math.h> 
+#include<locale.h> 
+void main()
+{
+	//task2
+	printf("Name -  PP-12-1”\n");
+	printf("Creator -  student\n");
+	printf("Size -  50MB\n");
+	printf("Path - D:\Academy\PP - 12 - 1\n");
+
+
+	//task1
+	printf("Name : “The war and the peace”\n");
+	printf("Avtor:  L.N. Tolstoj\n");
+	printf("Izdatelstvo:  Piter\n");
+	printf("Pages : 500\n");
+	
+	system("pause");
+}
